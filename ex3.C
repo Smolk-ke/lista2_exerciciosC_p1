3cbc0991084f9ed7c3b5d1a5cc1b6d661fdab93b
Antonio Santos <alvesantonio2407@gmail.com>
2026-04-16 13:14:24 -0300

    Exercício 3-3. faça um programa, que receba 4 notas de um aluno, e calcule a média, se a nota for maior que 7, imprima que o aluno está aprovado, se for entre 5 e 7, ele está de recuperação, se for menor que 5, ele está reprovado

---
+++ ex3.C
@@ -0,0 +1,9 @@
+/*3. faça um programa, que receba 4 notas de um aluno, e calcule a média, se a nota for
+maior que 7, imprima que o aluno está aprovado, se for entre 5 e 7, ele está de
+recuperação, se for menor que 5, ele está reprovado*/
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
