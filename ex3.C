45c57ddb3f318b56506ff7d0b577d1f8a0895ed3
Antonio Santos <alvesantonio2407@gmail.com>
2026-04-16 13:26:48 -0300

    Ex 3 completo

--- ex3.C
+++ ex3.C
@@ -4,6 +4,24 @@ recuperação, se for menor que 5, ele está reprovado*/
 #include <stdio.h>
 
 int main(){
+    float nota1, nota2, nota3, nota4, media;
 
+    printf("Informe a nota na 1ª avaliacao: ");
+    scanf("%f", &nota1);
+    printf("Informe a nota na 2ª avaliacao: ");
+    scanf("%f", &nota2);
+    printf("Informe a nota na 3ª avaliacao: ");
+    scanf("%f", &nota3);
+    printf("Informe a nota na 4ª avaliacao: ");
+    scanf("%f", &nota4);
+
+    media = (nota1+nota2+nota3+nota4)/4;
+    if(media>7){
+        printf("SITUACAO: APROVADO");
+    }else if(media>5){
+        printf("SITUACAO: RECUPERACAO");
+    }else{
+        printf("SITUACAO: REPROVADO");
+    }
     return 0;
 }
